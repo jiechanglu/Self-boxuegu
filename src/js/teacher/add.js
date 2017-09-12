db59e73f6056e5b5991464b22165e9c5fcc0632c
Jay <384234790@qq.com>
2017-09-12 11:14:58 +0800

    实现添加讲师功能

--- src/js/teacher/add.js
+++ src/js/teacher/add.js
@@ -1,2 +1,49 @@
 // 首页需要头部功能，所以这里导入一下
 require('../common/header.js');
+
+$('#add_btn').on('click', function() {
+    // var type = $('#tc_type').val();
+    // var datas = [
+    //     { "tc_name": $('#tc_name').val() },
+    //     { "tc_join_date": $('#tc_join_date').val() },
+    //     { "tc_type": type },
+    //     { "tc_gender": $("input[name='tc_gender']").val() }
+    // ];
+    // var dataStr = $("#techer_list_add").serialize()
+
+    // $.ajax({
+    //     type: 'post',
+    //     url: '/v6/teacher/add',
+    //     data: $("#techer_list_add").serialize(),
+    //     success: function(data) {
+    //         if ($('#tc_type').val() == 0) {
+    //             alert('无权限添加管理员，请重新添加！！');
+    //             location.href = '/dist/html/teacher/add.html';
+    //             return false;
+    //         }
+    //         if (data.code == 200) {
+    //             // $("#techer_list_add").ajaxSubmit($("#techer_list_add").serialize());
+    //             alert('添加成功');
+    //             location.href = '/dist/html/teacher/list.html';
+    //             return false;
+    //         }
+    //     }
+    // });
+    // return false;
+
+    if ($('#tc_type').val() == 0) {
+        alert('无权限添加管理员，请重新添加！！');
+        location.href = '/dist/html/teacher/add.html';
+        return false;
+    } else {
+        $('#techer_list_add').ajaxForm({
+            delegation: true,
+            success: function(data) {
+                if (data.code == 200) {
+                    alert('添加成功');
+                    location.href = '/dist/html/teacher/list.html';
+                }
+            }
+        });
+    }
+});
--- src/js/teacher/list.js
+++ src/js/teacher/list.js
@@ -1,2 +1,37 @@
-// 首页需要头部功能，所以这里导入一下
 require('../common/header.js');
+require('../common/aside.js');
+
+/**
+ * 功能点：
+ * 1、列表数据展示
+ * 2、讲师查看
+ * 3、讲师启用与注销
+ * */
+
+/**
+ * 列表数据展示：
+ * 1、请求接口获取数据
+ * 2、得到数据渲染后的模版，插入页面指定位置
+ * */
+$.get('/v6/teacher', function(data) {
+    data.code == 200 && $('#teacher-list-table').append(template('teacher-list-tpl', data.result));
+});
+
+/**
+ * 讲师启用与注销：
+ * 1、因为按钮是随着列表动态生成的，所以需要委托的方式绑定click事件
+ * 2、点击时通过自定义属性拿到这个按钮身上的tc_id与tc_status请求接口
+ * 3、状态修改成功后，要重新设置按钮的文本，按钮的自定义属性
+ * */
+$(document).on('click', '.btn-teacher-status', function() {
+    var $this = $(this); // 这里保存按钮是为了在ajax的回调中再次使用
+    var data = {
+        tc_id: $(this).attr('data-id'),
+        tc_status: $(this).attr('data-status')
+    };
+    $.post('/v6/teacher/handle', data, function(data) {
+        var newStatus = data.result.tc_status;
+        $this.text(newStatus == 0 ? '注 销' : '启 用'); // 更新按钮的文本
+        $this.attr('data-status', newStatus); // 修改自定位属性为新的status
+    });
+});
